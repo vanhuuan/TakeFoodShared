c21facab4234c48233bd579d5c2d228c5fe3eac0
Văn Hữu An <58378623+vanhuuan89@users.noreply.github.com>
2022-10-19 20:08:38 +0700

    add start up

---
+++ Startup.cs
@@ -0,0 +1,162 @@
+﻿using AuthenticationService.Extension;
+using AuthenticationService.Middleware;
+using AuthenticationService.Model.Entities.Role;
+using AuthenticationService.Model.Entities.User;
+using AuthenticationService.Model.Repository;
+using AuthenticationService.Service;
+using AuthenticationService.Service.Implement;
+using AuthenticationService.Settings;
+using Microsoft.AspNetCore.HttpOverrides;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace AuthenticationService;
+
+public class Startup
+{
+    /// <summary>
+    /// Start up
+    /// </summary>
+    /// <param name="env"></param>
+    public Startup(IWebHostEnvironment env)
+    {
+        try
+        {
+            var appSettingString = GetAppSettingString(env);
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{appSettingString}.json", optional: true)
+                .AddEnvironmentVariables("APPSETTING_");
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+        }
+    }
+
+    /// <summary>
+    /// Get AppSetting String
+    /// </summary>
+    /// <param name="env"></param>
+    /// <returns></returns>
+    private string GetAppSettingString(IWebHostEnvironment env)
+    {
+        string hostName = env.EnvironmentName;
+        if (!string.IsNullOrEmpty(hostName))
+        {
+            if (hostName.ToLower().Contains("test"))
+            {
+                return "test";
+            }
+            else if (hostName.ToLower().Contains("dev"))
+            {
+                return "dev";
+            }
+        }
+        return env.EnvironmentName;
+    }
+
+    /// <summary>
+    /// Configuration
+    /// </summary>
+    public IConfigurationRoot Configuration { get; }
+
+    /// <summary>
+    /// App setting
+    /// </summary>
+    private AppSetting appSetting { get; set; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        var appSettingsSection = Configuration.GetSection("AppSettings");
+        services.Configure<AppSetting>(appSettingsSection);
+        appSetting = appSettingsSection.Get<AppSetting>();
+
+        services.AddMvc((options) =>
+        {
+            options.EnableEndpointRouting = true;
+        }).AddJsonOptions((options) =>
+        {
+            options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+            options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        });
+
+        services.AddControllers();
+        services.AddSwaggerGen();
+        services.AddEndpointsApiExplorer();
+
+        string databaseName = appSetting.NoSQL.DatabaseName;
+        string mongoConnectionString = $"{appSetting.NoSQL?.ConnectionString}{appSetting.NoSQL?.ConnectionSetting}";
+
+        services.AddMongoDb(mongoConnectionString, databaseName);
+
+        services.AddSingleton(appSetting);
+
+        // setting serialize decimal data type to bson
+        BsonSerializer.RegisterSerializer(new DecimalSerializer(BsonType.Decimal128));
+
+        services.AddMongoRepository<User>(appSetting.NoSQL.Collections.User);
+        services.AddMongoRepository<Role>(appSetting.NoSQL.Collections.Role);
+        services.AddMongoRepository<UserRefreshToken>(appSetting.NoSQL.Collections.UserRefreshToken);
+        services.AddMongoRepository<Account>(appSetting.NoSQL.Collections.Account);
+
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<IMailService, MailService>();
+        services.AddScoped<IJwtService, JwtService>(x => new JwtService(x.GetRequiredService<IMongoRepository<UserRefreshToken>>()
+           , appSetting.JwtConfig.Secret, appSetting.JwtConfig.Secret2, appSetting.JwtConfig.ExpirationInHours, appSetting.JwtConfig.ExpirationInMonths));
+
+        services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(
+                builder =>
+                {
+                    builder.AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                    //.AllowCredentials();
+                }
+            );
+        });
+    }
+
+    /// <summary>
+    /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline
+    /// </summary>
+    /// <param name="app"></param>
+    public void Configure(IApplicationBuilder app)
+    {
+        try
+        {
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+            app.UseRouting();
+
+            app.UseMiddleware<AuthenticationMiddleware>();
+            // app.UseMiddleware<UserMiddleware>();
+
+            app.UseDefaultFiles();
+
+            app.UseStaticFiles();
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+}
